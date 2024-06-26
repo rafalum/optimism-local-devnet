378a374fd09175113456231d1bc72f1036c5ae13
rafalum <de.rafael@pm.me>
2024-06-26 14:38:33 +0200

    update installation process

--- local_node.sh
+++ local_node.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-sudo apt install -y make jq
+sudo apt update && sudo apt upgrade -y
 
-sudo apt update 
+# Basics
+sudo apt-get remove needrestart -y
+sudo apt install -y make jq
+sudo apt install build-essential -y
 sudo apt install python3-pip -y
 
-sudo apt update
-wget https://go.dev/dl/go1.20.linux-amd64.tar.gz
-tar xvzf go1.20.linux-amd64.tar.gz
-sudo cp go/bin/go /usr/bin/go
-sudo mv go /usr/lib
-echo export GOROOT=/usr/lib/go >> "$HOME/.bashrc"
+# Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-# Add Docker's official GPG key:
+# Docker
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-# Add the repository to Apt sources:
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
@@ -31,32 +29,38 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 echo docker compose '$*' | sudo tee /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose 
 
-curl -L https://foundry.paradigm.xyz | bash
+sudo usermod -aG docker sandbox
+newgrp docker
+exit
+
+# Node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source "$HOME/.bashrc"
-$HOME/.foundry/bin/foundryup
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install 16
+nvm use 16
 
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-
-sudo apt-get update
-sudo apt-get install nodejs -y
-
+# Pnpm
 curl -fsSL https://get.pnpm.io/install.sh | bash
 source "$HOME/.bashrc"
 
-echo 'export PATH="$PATH:$HOME/go/bin"' >> "$HOME/.bashrc"
-source "$HOME/.bashrc"
+# Go
+sudo apt update
+wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz
+tar xvzf go1.21.5.linux-amd64.tar.gz
+sudo cp go/bin/go /usr/bin/go
+sudo mv go /usr/lib
+echo export GOROOT=/usr/lib/go >> ~/.bashrc
+. ~/.bashrc
 
+# Optimism
 git clone https://github.com/ethereum-optimism/optimism.git
 cd optimism
-
 git submodule update --init --recursive
 
-sudo usermod -aG docker sandbox
-newgrp docker
+# Foundry
+pnpm install
+pnpm install:foundry
+
 
