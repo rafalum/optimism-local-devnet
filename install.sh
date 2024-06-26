34a5572671d0fb06e0d241bcb35de984434d9d39
rafalum <de.rafael@pm.me>
2024-06-26 14:38:50 +0200

    add permission to poweroff

--- install.sh
+++ install.sh
@@ -8,6 +8,12 @@ sudo apt-get install -q -y git
 # Install vim
 sudo apt install vim -y
 
+# Allow sandbox user to poweroff and reboot without password
+TMP_FILE=$(mktemp)
+trap 'rm -f $TMP_FILE' EXIT
+echo "sandbox ALL=(ALL) NOPASSWD: /sbin/poweroff, /sbin/reboot" >> $TMP_FILE
+sudo EDITOR="cat $TMP_FILE >>" visudo
+
 # Remove lines 4-9 from .bashrc
 sed -i '4,9d' "$HOME/.bashrc"
 
